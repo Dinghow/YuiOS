573a8c0a86b46243d1ed32e6dcadcdc4ebe0573e
Dinghow <DinghowYang@gmail.com>
2018-09-11 11:41:21 +0800

    Upload version1.1.0

--- kernel/welcome.c
+++ kernel/welcome.c
@@ -11,71 +11,35 @@
 #include "proto.h"
 
 void welcomeAnimation(){
-	int t=500;
-	printf("         t# ##.:E###K.        ,K##################G.             #f          \n");
+	int t=200;
 	milli_delay(t);
-	printf("         W#        .W##W.f###.                   ,f######:###  LD  #W        \n");
-	milli_delay(t);
-	printf("         K#           t####G                                      ##         \n");
-	milli_delay(t);
-	printf("         G#  :K#t                                        .E       W#         \n");
-	milli_delay(t);
-	printf("         i#       j;                                     #        ##         \n");
-	milli_delay(t);
-	printf("         ;#        .                                     K        ##         \n");
-	milli_delay(t);
-	printf("         .#       ,i                                     #       #D          \n");
-	milli_delay(t);
-	printf("          #                                               K      #i          \n");
-	milli_delay(t);
-	printf("          #       E                                         iE    #.         \n");
-	milli_delay(t);
-	printf("           #     f                                               :#          \n");
-	milli_delay(t);
-	printf("            #                                                     L#         \n");
-	milli_delay(t);
-	printf("          #                                         WD#          #E          \n");
-	milli_delay(t);
-	printf("         i#         #Wi                           #####K#          :#        \n");
-	milli_delay(t);
-	printf("         G#            ####E,                    #########           #       \n");
-	milli_delay(t);
-	printf("         #,           ###i                       #########          #L       \n");
-	milli_delay(t);
-	printf("         #.         t.                            #######           WK       \n");
-	milli_delay(t);
-	printf("         #                                         ,W#Wt         ##W##GLt;.  \n");
-	milli_delay(t);
-	printf("    W########WK                                                     i#       \n");
-	milli_delay(t);
-	printf("         #                                                       #,.,#       \n");
-	milli_delay(t);
-	printf("         #                             :i                          .K##Wj.   \n");
-	milli_delay(t);
-	printf("        .###Wj.                #       iK       #                   #W    tK \n");
-	milli_delay(t);
-	printf("    L##Wj#D                    #.      ##       #                   #,       \n");
-	milli_delay(t);
-	printf("         f#                      ####i#t######E                    #         \n");
-	milli_delay(t);
-	printf("          #j                                                      #i         \n");
-	milli_delay(t);
-	printf("            .###f                                               ##G          \n");
-	milli_delay(t);
-	printf("               W##                                            :G#            \n");
-	milli_delay(t);
-	printf("                  ###iK#######################################               \n");
-	milli_delay(t);
-	printf("\n");
-	milli_delay(t);
-	printf("\n");
-	milli_delay(t);
+	int i = 0;
+	for (i = 1; i <= 100; ++i)
+	{
+		printf("-");
+	}
+	for (i = 1; i <= 100; ++i)
+	{
+		printf("\b");
+	}
+	for (i = 1; i <= 240; ++i)
+	{
+		printf(">");
+		milli_delay(t);
+	}
+
 	printf("\n");
 	milli_delay(t);
 	printf("\n");
 }
 
-PUBLIC void welcome(){    
+PUBLIC void welcome(){   
+	printf("\n");
+       	printf("                          __  __      _ ____  _____                          \n");
+	printf("                          \\ \\/ /_  __(_) __ \\/ ___/                       \n");
+	printf("                           \\  / / / / / / / /\\__ \\                        \n");
+	printf("                           / / /_/ / / /_/ /___/ /                           \n");
+	printf("                          /_/\\__,_/_/\\____//____/                          \n");                                        
 	printf(" ___________________________________________________________________________ \n");
 	printf("|                                                                           |\n");
 	printf("|==================================YuiOS====================================|\n");
